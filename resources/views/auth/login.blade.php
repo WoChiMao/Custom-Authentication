033c3578dc4313fc7315b6d5a71968c83553b3f4
Arvi Jake De Guzman <arvijakedeguzman44@gmail.com>
2022-03-18 21:11:41 +0800

    Custom Authentication and Validation

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    </head>
+    <body class="container">
+        
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 mt-5">
+                <h4>LOGIN</h4>
+                <form action="{{ route('login-user') }}" method="post">
+                    @if (Session::has('success'))
+                        <div class="alert alert-success">{{ Session::get('success') }}</div>
+                    @endif
+                    @if (Session::has('fail'))
+                        <div class="alert alert-danger">{{ Session::get('fail') }}</div>
+                    @endif
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" placeholder="Enter email" name="email" value="{{ old('email') }}">
+                        <span class="text-danger">@error('email') {{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" placeholder="Enter password" name="password" value="{{ old('password') }}">
+                        <span class="text-danger">@error('password') {{ $message }} @enderror</span>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                    <br>
+                    <a href="registration">Register here!</a>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    </head>
+    <body class="container">
+        
+        <div class="row justify-content-end">
+            <div class="col">
+                <a href="logout"><i class="bi bi-box-arrow-right" style="font-size: 1rem; color: cornflowerblue;">logout</i></a>
+            </div>
+        </div>
+        
+        <div class="row pt-5">
+            <h3>Welcome This is a Custom Login and Register Authentication</h3>
+        </div>
+    </body>
+</html>
